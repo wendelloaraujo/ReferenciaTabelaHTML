ebc436098dd407e7f40ec22b78835155c31cc39e
Wendell Oliveira de Araujo <3295379@ifrn.local>
2024-05-22 13:54:44 -0300

    Referencia de Tabelas em HTML

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const botao = document.getElementById("botao");
-    const mensagem = document.getElementById("mensagem");
-
-    botao.addEventListener("click", function () {
-        mensagem.textContent = "Você clicou no botão!";
-    });
-});
